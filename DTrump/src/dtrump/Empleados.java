482aa6310cd767814ec819c9c092bb27dfe51d9e
Alexy Cruz <xytojr@gmail.com>
2016-05-11 18:05:59 -0600

    clases(revisar)

---
+++ DTrump/src/dtrump/Empleados.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dtrump;
+
+/**
+ *
+ * @author alexy
+ */
+public class Empleados {
+    String nombre;
+    int ID;
+    int edad;
+    String direccion;
+    int salario;
+
+    public Empleados() {
+    }
+
+    public Empleados(String nombre, int ID, int edad, String direccion, int salario) {
+        this.nombre = nombre;
+        this.ID = ID;
+        this.edad = edad;
+        this.direccion = direccion;
+        this.salario = salario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public int getSalario() {
+        return salario;
+    }
+
+    public void setSalario(int salario) {
+        this.salario = salario;
+    }
+
+    @Override
+    public String toString() {
+        return "Empleados{" + "nombre=" + nombre + ", ID=" + ID + ", edad=" + edad + ", direccion=" + direccion + ", salario=" + salario + '}';
+    }
+    
+}
---
+++ DTrump/src/dtrump/Materiales.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dtrump;
+
+/**
+ *
+ * @author alexy
+ */
+public class Materiales {
+    String nombre;
+    String descripcion;
+    String marca;
+    int ID;
+
+    public Materiales() {
+    }
+
+    public Materiales(String nombre, String descripcion, String marca, int ID) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.marca = marca;
+        this.ID = ID;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    @Override
+    public String toString() {
+        return "Materiales{" + "nombre=" + nombre + ", descripcion=" + descripcion + ", marca=" + marca + ", ID=" + ID + '}';
+    }
+    
+}
